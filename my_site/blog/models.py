eb9aa7218350f7b88b1e1c4ddc9ddea063c1be2e
Gautam Diwan <gautam.diwan@hcode.tech>
2024-03-17 20:05:38 +0530

    Add comments and Read Later functionality

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -33,3 +33,19 @@ class Post(models.Model):
     author = models.ForeignKey(
         Author, null=True, on_delete=models.SET_NULL, related_name="posts")
     tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
+    
+
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=120)
+    user_email = models.EmailField(max_length=254)
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.text
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,9 +1,13 @@
 from datetime import date
 
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.views.generic import ListView, DetailView
+from django.views import View
 
 from .models import Post
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -28,12 +32,70 @@ class AllPostsView(ListView):
     context_object_name = "all_posts"
 
 
-class SinglePostView(DetailView):
-    model = Post
-    template_name = "blog/post-details.html"
+class SinglePostView(View):
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        return context
-    
+    def is_stored_post(self, request, post_id):
+        stored_posts = request.session.get("stored_posts", [])
+        is_saved_for_later = post_id in stored_posts
+        return is_saved_for_later
+
+    def __render_context(self, request, post):
+
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_post(request, post.id)
+        }
+        return render(request, "blog/post-details.html", context)
+
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+
+        return self.__render_context(request, post)
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("post-details-page", args=[slug]))
+
+        return self.__render_context(request, post)
+
+
+class ReadLaterView(View):
+    def get(self, request):
+        stored_posts = request.session.get("stored_posts", [])
+        context = {}
+
+        if len(stored_posts) == 0:
+            context["posts"] = []
+            context["has_posts"] = False
+        else:
+            posts = Post.objects.filter(id__in=stored_posts)
+            context["posts"] = posts
+            context["has_posts"] = True
+
+        return render(request, "blog/stored-posts.html", context)
+
+    def post(self, request):
+        stored_posts = request.session.get("stored_posts", [])
+        post_id = int(request.POST["post_id"])
+
+        if post_id not in stored_posts:
+            stored_posts.append(post_id)
+            request.session["stored_posts"] = stored_posts
+            return HttpResponseRedirect("/")
+        else:
+            stored_posts.remove(post_id)
+            request.session["stored_posts"] = stored_posts
+            
+            current_post = Post.objects.get(id=post_id)
+            current_post_slug = current_post.slug
+
+            return HttpResponseRedirect(reverse("post-details-page", args=[current_post_slug]))
